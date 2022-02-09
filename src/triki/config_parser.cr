1d03c2fa3ebf8e8d0fb4a4fd14a313ea591c73c2
Isaiev <isaeffs06@gmail.com>
2022-02-09 20:37:32 +0200

    add :keep for tables by default

--- src/triki/config_parser.cr
+++ src/triki/config_parser.cr
@@ -1,7 +1,7 @@
 class Triki
   module ConfigParser
     def self.cast_bindings(bindings) : ConfigHash
-      type_hash = ConfigHash.new
+      type_hash = ConfigHash.new(:keep)
       bindings.each do |k, v|
         type_hash[k] = cast_table(v)
       end
